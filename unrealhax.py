dc9c58e9ae4fb9397d0db0e689ad4acc79251fe0
Ronitanand0000 <113673966+Ronitanand0000@users.noreply.github.com>
2024-10-06 08:41:31 +0530

    Update unrealhax.py

--- unrealhax.py
+++ unrealhax.py
@@ -101,8 +101,7 @@ async def attack(update: Update, context: CallbackContext):
         f"*⚔️ Attack Launched! ⚔️*\n"
         f"*🎯 Target: {ip}:{port}*\n"
         f"*🕒 Duration: {duration} seconds*\n"
-        f"*🔥 Enjoy And Kill Whole Lobby  💥
-        Owner - @RONIT_IN*"
+        f"*🔥 Enjoy And Kill Whole Lobby  💥 Owner - @RONIT_IN 💥*"
     ), parse_mode='Markdown')
 
     asyncio.create_task(run_attack(chat_id, ip, port, duration, context))
